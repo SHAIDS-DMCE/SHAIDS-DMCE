8fba1a645db0f82aa675f360a415d4ee9c012dc5
Yash Bhangale <68957369+yashbhangale@users.noreply.github.com>
2024-07-24 13:16:51 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+const GroupLink = () => {
+window.open("#")
+}
+
+// loader 
+window.addEventListener('load', function() {
+    var loadingAnimation = document.getElementById('loading-animation');
+    loadingAnimation.style.display = 'none';
+  });
+  
